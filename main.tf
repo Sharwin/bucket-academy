d6836057b85fa56c853ac2ef50eda4e8533c8c68
Ivan <idbellog@gmail.com>
2026-04-24 16:49:00 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_s3_bucket" "academy-bucket" {
+  bucket = "academy-bucket-glb"
+
+  tags = {
+    academy     = "terraform202601"
+    Environment = "Dev"
+  }
+}
