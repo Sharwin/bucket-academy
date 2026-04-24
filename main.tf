9c4a8a03d7422d8dcef3da4fe32ac4843e2b37ca
Ivan <idbellog@gmail.com>
2026-04-24 17:01:38 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
 provider "aws" {
   region  = "us-east-1"
 }
-
-resource "aws_s3_bucket" "academy-bucket" {
-  bucket = "academy-bucket-glb"
-
-  tags = {
-    academy     = "terraform202601"
-    Environment = "latest"
-  }
-}
